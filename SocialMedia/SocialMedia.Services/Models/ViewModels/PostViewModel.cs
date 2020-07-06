759590b49b017e2df02f37c5ccfa7f07b8aac844
yavaka <yavakaa@gmail.com>
2020-07-06 10:50:28 +0100

    Add PostViewModel

---
+++ SocialMedia/SocialMedia.Services/Models/ViewModels/PostViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace SocialMedia.Services.Models.ViewModels
+{
+    using SocialMedia.Data.Models;
+    using System.Collections.Generic;
+
+    public class PostViewModel
+    {
+        public PostViewModel()
+        {
+            this.Friends = new List<UserServiceModel>();
+            this.TaggedFriends = new List<UserServiceModel>();
+        }
+
+        public Post Post { get; set; }
+
+        public UserServiceModel CurrentUser { get; set; }
+
+        public ICollection<UserServiceModel> Friends { get; set; }
+     
+        public ICollection<UserServiceModel> TaggedFriends { get; set; }
+    }
+}
